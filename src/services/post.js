a2dbe8b9224cbc05ea391d18208a11f6efd2faaa
vietduc01100001 <vietduc01100001@gmail.com>
2019-10-26 16:56:42 +0700

    edit queries

--- src/services/post.js
+++ src/services/post.js
@@ -2,7 +2,14 @@ const { log } = require('@vietduc/common');
 const postgres = require('@vietduc/postgres');
 
 const findAll = async () => {
-    const sql = `SELECT * FROM posts;`;
+    const sql = `SELECT
+        id,
+        title,
+        TO_CHAR(created_at, 'dd/mm/yyyy') AS "createdAt",
+        tags,
+        content,
+        status
+        FROM posts;`;
     const { err, res } = await postgres.exec(sql);
     if (err || !Array.isArray(res)) {
         if (err) log.error('Error querying posts');
@@ -14,7 +21,15 @@ const findAll = async () => {
 module.exports.findAll = findAll;
 
 const findOne = async ({ id }) => {
-    const sql = `SELECT * FROM posts WHERE id = '${id}';`;
+    const sql = `SELECT
+        id,
+        title,
+        TO_CHAR(created_at, 'dd/mm/yyyy') AS "createdAt",
+        tags,
+        content,
+        status
+        FROM posts
+        WHERE id = '${id}';`;
     const { err, res } = await postgres.exec(sql);
     if (err || !Array.isArray(res) || !res[0]) {
         if (err) log.error('Error querying posts');
